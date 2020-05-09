60fc8b39a77b4252c14c6efddc4b9f34fb248f09
Cristian Torres <ctorresthrash@gmail.com>
2020-05-09 06:53:15 -0500

    add reminders by date store

--- src/stores/reminders.js
+++ src/stores/reminders.js
@@ -1,5 +1,12 @@
-import { writable } from "svelte/store";
+import { writable, derived } from "svelte/store";
+import _ from "lodash/fp";
+import { format } from "date-fns";
 
 export const reminders = writable({});
 
-
+export const remindersByDate = derived(reminders, ($reminders) => {
+  const groupedByDateReminders = _.groupBy((reminder) => {
+    return format(reminder.date, "MM/dd/YYYY");
+  }, $reminders);
+  return groupedByDateReminders;
+});
