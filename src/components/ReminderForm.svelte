8cda99867e79ba32e27dc6f5b168b88963090250
Cristian Torres <ctorresthrash@gmail.com>
2020-05-09 13:50:42 -0500

    add validation ReminderForm component

--- src/components/ReminderForm.svelte
+++ src/components/ReminderForm.svelte
@@ -2,20 +2,26 @@
   import { createForm } from "svelte-forms-lib";
   import Button from "./Button.svelte";
   import TextInput from "./TextInput.svelte";
+  import * as Yup from "yup";
 
   export let onSubmit = () => {};
 
-  const { form, handleChange, handleSubmit } = createForm({
+  const { form, handleChange, handleSubmit, errors } = createForm({
     initialValues: {
       content: "",
       date: "",
       city: ""
     },
+    validationSchema: Yup.object().shape({
+      content: Yup.string().required("Required"),
+      date: Yup.date().required("Required").typeError('Invalid date'),
+      city: Yup.string().required("Required")
+    }),
     onSubmit: values => {
-      console.log(values);
       onSubmit(values);
     }
   });
+  $: console.log($errors);
 </script>
 
 <form class="bg-white px-8 pt-6 pb-8 mb-4" on:submit={handleSubmit}>
@@ -25,7 +31,8 @@
       name="content"
       label="Content"
       on:change={handleChange}
-      value={$form.content} />
+      value={$form.content}
+      error={$errors.content} />
   </div>
   <div class="mb-4">
     <TextInput
@@ -34,7 +41,8 @@
       label="Date"
       type="datetime-local"
       on:change={handleChange}
-      value={$form.date} />
+      value={$form.date}
+      error={$errors.date} />
   </div>
   <div class="mb-6">
     <TextInput
@@ -42,7 +50,8 @@
       name="city"
       label="City"
       on:change={handleChange}
-      value={$form.city} />
+      value={$form.city}
+      error={$errors.city} />
   </div>
 
   <Button type="submit">Submit</Button>
